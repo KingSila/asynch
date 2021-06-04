2f26cd6f3714e7894163aef0500228a6d5027339
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2021-06-04 17:32:08 +0200

    commit

---
+++ app3.js
@@ -0,0 +1,20 @@
+const cookBeanSouffle = require('./library.js');
+
+// Write your code below:
+async function hostDinnerParty(){
+  try{
+    let value = await cookBeanSouffle();
+  console.log(value + ' is served!')
+  }
+  catch(error){
+    console.log(error);
+    console.log('Ordering a pizza!');
+
+  }
+}
+hostDinnerParty();
+
+
+
+
+
